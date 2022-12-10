88ebb8e6d83d27535aeeab938f77fd1afc53ab74
WillTDP <77252197+WillTDP@users.noreply.github.com>
2022-12-10 15:38:40 +0100

    FIX: Fully replaced the Vite install because Vue wasn't working FIX: Migrated the needed parts of backend.html to a vue component and moved to backendvue.html to handle the browser page

---
+++ donuttello/src/components/OrderDetails.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="container">
+        <p class="customer_name">Customer name</p>
+        <img class="donut" src="assets/images/donut_img.png" alt="">
+        <div class="aantal">
+          <p class="aantal-title">Aantal</p>
+          <p class="aantal-thing">placeholder aantal</p>
+        </div>
+        <div class="glazuur">
+          <p class="glazuur glazuur-title">Glazuur</p>
+          <p class="glazuur glazuur-thing">G</p>
+        </div>
+        <div class="logo">
+          <p class="logo-title">Logo</p>
+          <img class="logo" src="assets/images/donut_img.png" alt="">
+        </div>
+        <div class="topping">
+          <p class="topping-title">Topping</p>
+          <p class="topping-thing">placeholder topping</p>
+        </div>
+        <div class="email">
+          <p class="email-title">E-Mail</p>
+          <p class="email-thing">placeholder email</p>
+        </div>
+        <div class="status">
+          <p class="status-title">Status</p>
+          <div>
+            <p class="status-thing">In behandeling</p>
+            <p class="status-thing">Afgehandeld</p>
+          </div>
+        </div>  
+      </div>
+</template>

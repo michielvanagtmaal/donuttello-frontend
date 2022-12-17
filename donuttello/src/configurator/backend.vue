0c5663992995036f0b86f1c6c585693d0e884489
WillTDP <77252197+WillTDP@users.noreply.github.com>
2022-12-17 17:51:08 +0100

    FIX: hopefully fixed the vue file. DonutConfig isn't used anymore because it's just a HMTL structure that the ThreeJS vue file needs and it broke when the config was its own file

--- donuttello/src/configurator/backend.vue
+++ donuttello/src/configurator/backend.vue
@@ -1,12 +1,12 @@
 <script setup>
-  import DonutConfig from './components/DonutConfig.vue'
+  //import DonutConfig from './components/DonutConfig.vue'
   import ThreeD from './components/DonutThreeD.vue'
 
 </script>
 
 <template>
   <div class="app">
-    <DonutConfig class="order" />
+    <!--<DonutConfig class="order" />-->
     <ThreeD class="order" />
   </div>
 </template>
